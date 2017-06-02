cd931ff3c1e9e2bb02b51708fe9bda1b9949d248
Ben Childs <benjamin.t.childs@gmail.com>
2017-06-02 01:09:20 -0500

    Have applied basics, partway through extra credits. Need to move on.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -30,15 +30,21 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
       var productsICanEat = [];
 
-      /* solve using filter() & all() / any() */
+      //takes in ingredient array
+      var hasShrooms = function(ingred){ return ingred === "mushrooms" };
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      /* solve using filter() & all() / any() */
+      productsICanEat = products.filter(function(prod){
+        return (!prod.containsNuts  && !( _(prod.ingredients).any(hasShrooms) ) );
+      });
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -52,13 +58,15 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _.chain(_.range(1000).filter( function(x){return (x % 5 === 0 || x % 3 === 0);} ))
+                .reduce(function(sum, x) { return sum + x })
+                .value();  /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -71,39 +79,115 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
+    var ingArrays = [];
 
     /* chain() together map(), flatten() and reduce() */
+    //map to pull all ingredient arrays
+    //flatten to reduce the resulting array of arrays to one
+    //reduce to sum all the ingredient counts into ingredientCount
+    
+    
+    _(products).chain()
+      .map(function(prod) { return prod.ingredients })
+      .flatten() 
+      .reduce(function(sum, ingredient) { return ingredientCount[ingredient] = ( ingredientCount[ingredient] || 0 ) + 1 })
+      .value();
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+      expect(ingredientCount['mushrooms']).toBe(2);
+
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
   
+  it("should find the largest prime factor of a composite number", function () {
+    //get array of all factors (ignore self and 1, since if none of the other factors are prime, it is itself prime)
+    //go through array and determine which (if any) factors are prime, replacing pointer to highest as necessary
+
+    //to find prime: iterate across numbers < factor starting at 2. If factor % i === 0, not prime
+    var x = 71;
+    var factors = [x];
+    var largestPrime = 1;
+    var increment;
+    var start;
+    var findLargerPrime = function(n){
+      //takes a number, determines if it is prime, and replaces largestPrime if it is prime and larger
+      var isPrime = true;
+      for(var i = 2; i < n; i++){
+        if(n % i === 0){isPrime = false;}
+      }
+
+      if(isPrime && n > largestPrime) {largestPrime = n;}
+
+    }
+
+    //to get factors, if odd we do every other number. If even we do every number. Only need to go up to half.
+    var half = Math.floor(x / 2);
+    x % 2 === 0 ? (start = 2, increment = 1) : (start = 3, increment = 2);
+
+    for(start; start <= half; start += increment){
+      //do not worry about efficiency, just do every number up to half
+      if( x % start === 0){ factors.push(start); }
+    }
+
+    if(factors.length === 1){
+      largestPrime = x;
+    } else {
+      for( var i = 0; i < factors.length; i++ ){
+
+      }
+    }
+
+    _(factors).forEach(findLargerPrime);
+
+    expect(largestPrime).toBe(71);
+
   });
 
-  it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-    
-  });
+  /*it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
+    //smallest number is 10,000 (100 * 100) and largest is 998,001 (999 * 999)
+    //to find palindromes: 
+          Step 1:
 
-  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-      
+          Start with any number. Call it original number. Reverse the digits of the original number
+
+          Step 2:
+
+          Call the number whose digits are reversed new number. Add the new number to your original number.
+
+          Call the number found by adding the new number to the original number test number
+
+          Step 3:
+
+          If test number is a palindrome, you are done. If not, use your test number as your original number and repeat the steps above
+  });*/
+
+  /*it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
+      //find least common multiple of 1-20
+      //break 1-20 into arrays of prime factors, count the greatest number of times each factor appears, multiply all those together
     
-  });
+  });*/
 
   it("should find the difference between the sum of the squares and the square of the sums", function () {
-    
-  });
+    var a = 20;
+    var b = 10;
 
+    var diff = Math.abs((Math.pow(a,2)+ Math.pow(b,2)) - Math.pow(a*b,2));
+
+    expect(diff).toBe(39500);
+
+  });
+/*
   it("should find the 10001st prime", function () {
+    //The 10,001st prime is 104,743.
+    //could do with a very long while loop, measuring the length of an array...
+    //sieve of eratosthenes
 
-  });
-  */
+  });*/
+  
 });
